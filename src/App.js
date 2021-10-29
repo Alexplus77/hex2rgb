174a46db9946f92fa352d8856ff7fac767709514
alexs <alexplus77@mail.ru>
2021-10-29 18:54:24 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -2,31 +2,21 @@ import "./App.css";
 import React, { useState } from "react";
 
 function App() {
-  const [form, setForm] = useState({ backgroundColor: "" });
-  const [stileColor, setStileColor] = useState({});
-
+  const [form, setForm] = useState("");
   const handleChange = ({ target }) => {
-    setForm({ ...form, [target.name]: target.value });
+    setForm(target.value);
   };
+  console.log(form);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setStileColor({ ...stileColor, ...form });
-  };
   return (
-    <div className="App" style={stileColor}>
-      <form onSubmit={handleSubmit}>
+    <div className="App" style={{ background: `${form}` }}>
+      <form>
         <input
           name="backgroundColor"
           value={form.backgroundColor}
           onChange={handleChange}
         />
-        <input
-          name="output"
-          readOnly={true}
-          defaultValue={stileColor.backgroundColor}
-        />
-        <input type="submit" value="submit" style={{ display: "none" }} />
+        <input name="output" readOnly={true} defaultValue={form} />
       </form>
     </div>
   );
