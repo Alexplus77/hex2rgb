318000ef685ca3bbe021a04bcac5bd5364ad360e
alexs <alexplus77@mail.ru>
2021-10-30 12:01:20 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import React, { useState } from "react";
 import hexToRgb from "hex-to-rgb";
 
 function App() {
-  const [form, setForm] = useState("");
+  const error = "Ошибка";
+  const validNumber = 7;
 
-  const handleChange = ({ target }) => {
-    setForm(target.value);
+  const [form, setForm] = useState("");
+  const [valid, setValid] = useState(true);
+
+  const RegExp = /^#[0-9A-F]{6}$/i;
+  const validateValue = (value) =>
+    RegExp.test(value) ? setValid(value) : setValid(RegExp.test(value));
+
+  const handleChange = ({ target: { value } }) => {
+    value.length === validNumber && validateValue(value);
+    setForm(value);
   };
 
   return (
-    <div className="App" style={{ background: `${form}` }}>
+    <div
+      className="App"
+      style={valid ? { background: `${valid}` } : { background: "#ff0000" }}
+    >
       <form>
         <input name="backgroundColor" value={form} onChange={handleChange} />
         <input
           name="output"
           readOnly={true}
-          value={`rgb (${hexToRgb(form)})`}
+          value={valid ? `rgb (${hexToRgb(valid)})` : error}
         />
       </form>
     </div>
